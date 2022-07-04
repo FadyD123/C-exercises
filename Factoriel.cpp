125601a23df28ad133e788377197030b1a41a50a
Fady Dawra <75436756+FadyD123@users.noreply.github.com>
2022-07-04 17:59:16 +0200

    Update Factoriel.cpp

--- Factoriel.cpp
+++ Factoriel.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 
-int factioriel(int n)
+int factoriel(int n)
 {
-    int i;
     int fact = 1;
     if (n == 0)
     {
@@ -25,21 +24,66 @@ int factioriel(int n)
 
 double combinaison(int n, int p)
 {
-    int factn = factioriel(n);
-    int factp = factioriel(p);
-    int fact_n_moins_p = factioriel(n - p);
-    double c = factn / (factp * fact_n_moins_p);
-    return c;
+    if (n < 0 || p < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        int factn = factoriel(n);
+        int factp = factoriel(p);
+        int fact_n_moins_p = factoriel(n - p);
+        double c = factn / (factp * fact_n_moins_p);
+        return c;
+    }
+}
+
+int solution_number_second_degree_eq(double a, double b, double c, double &delta)
+{
+    delta = b * b - 4 * a * c;
+    if (delta < 0)
+        return 0;
+    else
+        if (delta == 0)
+            return 1;
+        else
+            return 2;
+}
+
+void solutions_second_degree_eq(double a, double b,double c)
+{
+    double delta;
+    int nb_sol = solution_number_second_degree_eq(a, b, c, delta);
+    double x, x1, x2;
+
+    switch (nb_sol)
+    {
+      case 0: cout << "no solution";
+         break;
+      case 1: 
+          x = -b / (2 * a);
+          cout << "we have one solution : " << x << endl;
+          break;
+      case 2: 
+          x1 = (-b + sqrt(delta)) / (2 * a);
+          x2 = (-b - sqrt(delta)) / (2 * a);
+          cout << "First solution : " << x1 << endl;
+          cout << "Second Solution : " << x2 << endl;
+    }
+
 }
 
 int main()
 {
-    cout << "n : ";
-    int n;
-    cin >> n;
-    cout << "p : ";
-    int p;
-    cin >> p;
-    cout << "nCp = " << combinaison(n, p) << endl;
+    cout << "a : ";
+    int a;
+    cin >> a;
+    cout << "b : ";
+    int b;
+    cin >> b;
+    cout << "c : ";
+    int c;
+    cin >> c;
+    solutions_second_degree_eq(a, b, c);
     return 0;
 }
